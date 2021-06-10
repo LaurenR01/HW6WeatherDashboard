976388babca02cdd8705148936aaf71cb5de3371
LaurenR01 <78819957+LaurenR01@users.noreply.github.com>
2021-06-09 22:34:43 -0400

    more html structure and jquery compatibility

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,40 @@
-// set api base URL and key
-const api = {
-    key: '5eb289bb53abf02581c3494d598b36af',
-    baseurl: 'https://api.openweathermap.org/data/2.5/'
-}
-// set what happens when a user submits their zip code
-const searchButton = document.querySelector('#submit')
-searchButton.addEventListener('click', setLocation);
+// setting the API key
 
-function setLocation(event){
-    getLocation(searchButton.value);
-    localStorage.setItem('location', searchButton.value );
-    console.log(searchButton.value);
-}
-
-function getLocation (query) {
-    fetch (`${api.baseurl}weather?zip=${searchButton.value}&units=imperial&appid=${api.key}`)
-    .then (weather =>{
-        return weather.json();
-    }).then (displayCurrent);
-}
-function displayCurrent(weather){
-    console.log(weather);
-    let location = document.querySelector('.display-panel');
-    location.innerText = `${weather.name}, ${weather.sys.country}`;
-}
+    const api = {
+        key: '5eb289bb53abf02581c3494d598b36af',
+        base: 'http://api.openweathermap.org/data/2.5/'
+    }
+    // capturing zip code
+    var store = 0;
+    var button = $(".submitBtn");
+    $(button).on('click', function(event){
+        zipcode = $("#zip-code").val();
+        console.log(zipcode);
+        localStorage.setItem(store, zipcode);
+        for (i=0; i<8; i++){
+            store = store+ 1;
+        }
+        getWeather(zipcode);
+    });
+    
+    // grabbing weather info from the API
+    function getWeather(zipcode) {
+        fetch (`${api.base}weather?zip=${zipcode}&units=imperial&appid=${api.key}`)
+        .then (response => {
+            return response.json();})
+        .then(showWeather);
+    }
+    
+    // show weather on screen
+    function showWeather (response) {
+            console.log(response);
+            let location = document.querySelector('.location');
+            location.innerText = response.name;
+            let currentTemp = document.querySelector('.current-temp');
+            currentTemp.innerText = `${response.main.temp}°F`;
+            let highLow =  document.querySelector('.high-low');
+            highLow.innerHTML = `${response.main.temp_max}°F / ${response.main.temp_min}°F`;
+            let description = document.querySelector('.description');
+            description.innerText = response.weather[0].description;
+        }
+    // })
